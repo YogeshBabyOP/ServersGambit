f7f2c970560242c27ed79600beb29f94c84bdc57
Yogesh <114099821+YogeshBabyOP@users.noreply.github.com>
2023-06-21 21:43:58 +0530

    Update main.php

--- php_server/main.php
+++ php_server/main.php
@@ -1,5 +1,4 @@
 <?php
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"];
     echo $username;
@@ -7,20 +6,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 else {
     $initialTime = microtime(true);
     $arraysize = 1000;
-
     $sum = 0;
     for ($i = 0; $i < $arraysize; $i++) {
         $sum += ($i + 1);
     }
-    for ($i = 0; $i < $arraysize; $i++) {
-    //;
-    }
     $finalTime = microtime(true);
-
     $timeTaken = $finalTime - $initialTime;
-
     echo "Time taken: $timeTaken seconds";
-
 }
-
 ?>
