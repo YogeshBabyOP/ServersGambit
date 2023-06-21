0d976b9af2319e569fbe4353a134489a2b59f5c7
Yogesh <114099821+YogeshBabyOP@users.noreply.github.com>
2023-06-21 21:47:38 +0530

    Update main.java

--- java_server/main.java
+++ java_server/main.java
@@ -1,5 +1,39 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.Println("hiGOD");
+    public static void main(String[] args) throws IOException {
+        System.out.println("running on http://localhost:8088/time");
+        HttpServer server = HttpServer.create(new InetSocketAddress(8088), 0);
+        server.createContext("/time", new TimeHandler());
+        server.setExecutor(null);
+        server.start();
+    }
+
+    static class TimeHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            long startTime = System.currentTimeMillis();
+            int[] array = new int[10000000];
+            for (int i = 0; i < array.length; i++) {
+                array[i] = i;
+            }
+            long sum = 0;
+            for (int value : array) {
+                sum += value;
+            }
+            long endTime = System.currentTimeMillis() - startTime;
+
+            String response = "JavaGOD time " + endTime;
+
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
     }
 }
